04f9494063406d35f537b108409f2e38a8bef42a
OLINY Damien <dodevup@gmail.com>
2019-07-07 15:21:14 -0400

    katex plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,60 @@
-const { CheckerPlugin } = require('awesome-typescript-loader');
-const LiveReloadPlugin = require('webpack-livereload-plugin');
-const path = require('path');
-const swag = require('@ephox/swag');
+const { CheckerPlugin } = require("awesome-typescript-loader");
+const LiveReloadPlugin = require("webpack-livereload-plugin");
+const path = require("path");
+const swag = require("@ephox/swag");
 
 module.exports = function(grunt) {
-  var packageData = grunt.file.readJSON('package.json');
-  var BUILD_VERSION = packageData.version + '-' + (process.env.BUILD_NUMBER ? process.env.BUILD_NUMBER : '0');
-  const libPluginPath = 'lib/main/ts/Plugin.js';
-  const scratchPluginPath = 'scratch/compiled/plugin.js';
-  const scratchPluginMinPath = 'scratch/compiled/plugin.min.js';
-  const tsDemoSourceFile = path.resolve('src/demo/ts/Demo.ts');
-  const jsDemoDestFile = path.resolve('scratch/compiled/demo.js');
+  var packageData = grunt.file.readJSON("package.json");
+  var BUILD_VERSION =
+    packageData.version +
+    "-" +
+    (process.env.BUILD_NUMBER ? process.env.BUILD_NUMBER : "0");
+  const libPluginPath = "lib/main/ts/Plugin.js";
+  const scratchPluginPath = "scratch/compiled/plugin.js";
+  const scratchPluginMinPath = "scratch/compiled/plugin.min.js";
+  const tsDemoSourceFile = path.resolve("src/demo/ts/Demo.ts");
+  const jsDemoDestFile = path.resolve("scratch/compiled/demo.js");
 
   grunt.initConfig({
     pkg: packageData,
 
     clean: {
-      dirs: [ 'dist', 'scratch' ]
+      dirs: ["dist", "../../pwa/src/assets/tinymce/plugins/latex/", "scratch"],
+      options: {
+        force: true
+      }
     },
 
     tslint: {
       options: {
-        configuration: 'tslint.json'
+        configuration: "tslint.json"
       },
-      plugin: ['src/**/*.ts']
+      plugin: ["src/**/*.ts"]
     },
 
     shell: {
-      command: 'tsc'
+      command: "tsc"
     },
 
     rollup: {
       options: {
         treeshake: true,
-        name: 'plugin',
-        format: 'iife',
-        banner: '(function () {',
-        footer: 'plugin();})();',
-        onwarn: swag.onwarn,
+        name: "plugin",
+        format: "iife",
+        banner: "(function () {",
+        footer: "plugin();})();",
+        onwarn: (warning, warn) => {
+          if (warning.code === "THIS_IS_UNDEFINED") return;
+          else swag.onwarn(warning);
+        },
         plugins: [
           swag.nodeResolve({
             basedir: __dirname,
             prefixes: {}
           }),
           swag.remapImports()
-        ]
+        ],
+        external: ["katex"]
       },
       plugin: {
         files: [
@@ -72,8 +82,8 @@ module.exports = function(grunt) {
         options: {
           process: function(src) {
             var buildSuffix = process.env.BUILD_NUMBER
-              ? '-' + process.env.BUILD_NUMBER
-              : '';
+              ? "-" + process.env.BUILD_NUMBER
+              : "";
             return src.replace(
               /@BUILD_NUMBER@/g,
               packageData.version + buildSuffix
@@ -82,12 +92,12 @@ module.exports = function(grunt) {
         },
         // scratchPluginMinPath is used twice on purpose, all outputs will be minified for premium plugins
         files: {
-          'dist/latex/plugin.js': [
-            'src/text/license-header.js',
+          "../../pwa/src/assets/tinymce/plugins/latex/plugin.js": [
+            "src/text/license-header.js",
             scratchPluginMinPath
           ],
-          'dist/latex/plugin.min.js': [
-            'src/text/license-header.js',
+          "../../pwa/src/assets/tinymce/plugins/latex/plugin.min.js": [
+            "src/text/license-header.js",
             scratchPluginMinPath
           ]
         }
@@ -98,41 +108,51 @@ module.exports = function(grunt) {
       css: {
         files: [
           {
-            cwd: 'src/text',
-            src: ['license.txt'],
-            dest: 'dist/latex',
+            cwd: "src/text",
+            src: ["license.txt"],
+            dest: "../../pwa/src/assets/tinymce/plugins/latex",
             expand: true
           },
-          { src: ['changelog.txt'], dest: 'dist/latex', expand: true }
+          {
+            src: ["changelog.txt"],
+            dest: "../../pwa/src/assets/tinymce/plugins/latex",
+            expand: true
+          },
+          {
+            cwd: "src/assets", // set working folder / root to copy
+            src: "**/*", // copy all files and subfolders
+            dest: "../../pwa/src/assets/tinymce/plugins/latex/assets", // destination folder
+            expand: true // required when using cwd
+          }
         ]
       }
     },
 
     webpack: {
       options: {
-        mode: 'development',
+        mode: "development",
         watch: true
       },
       dev: {
         entry: tsDemoSourceFile,
-        devtool: 'source-map',
+        devtool: "source-map",
 
         resolve: {
-          extensions: ['.ts', '.js']
+          extensions: [".ts", ".js"]
         },
 
         module: {
           rules: [
             {
               test: /\.js$/,
-              use: ['source-map-loader'],
-              enforce: 'pre'
+              use: ["source-map-loader"],
+              enforce: "pre"
             },
             {
               test: /\.ts$/,
               use: [
                 {
-                  loader: 'ts-loader',
+                  loader: "ts-loader",
                   options: {
                     transpileOnly: true,
                     experimentalWatchApi: true
@@ -153,21 +173,24 @@ module.exports = function(grunt) {
     }
   });
 
-  require('load-grunt-tasks')(grunt);
-  grunt.loadNpmTasks('@ephox/swag');
+  require("load-grunt-tasks")(grunt);
+  grunt.loadNpmTasks("@ephox/swag");
 
-  grunt.registerTask('version', 'Creates a version file', function () {
-    grunt.file.write('dist/latex/version.txt', BUILD_VERSION);
+  grunt.registerTask("version", "Creates a version file", function() {
+    grunt.file.write(
+      "../../pwa/src/assets/tinymce/plugins/latex/version.txt",
+      BUILD_VERSION
+    );
   });
 
-  grunt.registerTask('default', [
-    'clean',
-    'tslint',
-    'shell',
-    'rollup',
-    'uglify',
-    'concat',
-    'copy',
-    'version'
+  grunt.registerTask("default", [
+    "clean",
+    "tslint",
+    "shell",
+    "rollup",
+    "uglify",
+    "concat",
+    "copy",
+    "version"
   ]);
 };
--- src/main/ts/Plugin.ts
+++ src/main/ts/Plugin.ts
@@ -1,17 +1,259 @@
+import { document, HTMLElement, alert } from "@ephox/dom-globals";
+import { Editor } from "tinymce";
+import * as katex from "katex";
+
 declare const tinymce: any;
 
-const setup = (editor, url) => {
-  editor.addButton('latex', {
-    text: 'latex button',
+const setup = (editor: Editor, url) => {
+  const $$ = tinymce.dom.DomQuery;
+  let jQuery;
+  // sets the cursor to the specified element, ed ist the editor instance
+  // start defines if the cursor is to be set at the start or at the end
+  function setCursor(ed, element, start) {
+    const doc = ed.getDoc();
+    if (typeof doc.createRange !== "undefined") {
+      const range = doc.createRange();
+      range.selectNodeContents(element);
+      range.collapse(start);
+      const win = doc.defaultView || doc.parentWindow;
+      const sel = win.getSelection();
+      sel.removeAllRanges();
+      sel.addRange(range);
+    } else if (typeof doc.body.createTextRange !== "undefined") {
+      const textRange = doc.body.createTextRange();
+      textRange.moveToElementText(element);
+      textRange.collapse(start);
+      textRange.select();
+    }
+  }
+
+  /* editor.addButton("separator", {
+
+    icon: false,
+    onclick: () => {
+      editor.execCommand(
+        "mceInsertContent",
+        false,
+        `<p style="line-height:1px; width: 100%; border-top:1px solid #636363; margin:0; padding: 0;"></p><p></p>`
+      );
+      editor.insertContent(`<p></p>`);
+    }
+  }); */
+
+  editor.on("init", function(event) {
+    jQuery = editor.getWin().parent.jQuery;
+  });
+
+  editor.on("change", function(e) {
+    console.log(e);
+    console.log(e.element);
+  });
+
+  editor.addButton("latex", {
+    text: "katex",
+    classes: "katex-editor",
+    // image: tinymce.baseURL + "/plugins/separator/assets/icons/separator.png",
     icon: false,
     onclick: () => {
       // tslint:disable-next-line:no-console
-      editor.setContent('<p>content added from latex</p>');
+      create_katex_editor();
+      // createEquation();
     }
   });
+
+  /* const insert_equation = (container: HTMLElement) => {
+    const equ = "\\left(x+y\\right)^n=\\sum_{k=0}^{n} \\binom{n}{k}x^ky^{n-k}";
+
+    container.innerHTML = `
+    <span class="math">
+    ${equ}
+    </span>
+  `.trim();
+    container.focus();
+
+    rerenderLaTeX();
+  };
+
+  const rerenderLaTeX = () => {
+    const body = editor.dom.getRoot();
+    const spans = $$(body).find("span.math");
+
+    console.log("Rendering katex, found " + spans.length + " spans");
+
+    spans.each(index => {
+      const span = spans[index];
+      const equation = span.innerHTML;
+
+      katex.render(equation, span, { displayMode: true });
+    });
+  }; */
+
+  const on_editor_node_change = function(e) {
+    if (!e) {
+      return;
+    }
+    const element: HTMLElement = e.element;
+    if (!element) {
+      return;
+    }
+
+    // check editor activeness
+    const isEditor = findUpClass(element, "katex_outer");
+    if (!isEditor) {
+      console.log(">editor is not active");
+      jQuery(".mce-katex-editor").removeClass("mce-active");
+      disable_katex_editor();
+    } else {
+      console.log(">editor is active");
+      jQuery(".mce-katex-editor").addClass("mce-active");
+    }
+
+    // check if editor should be active
+    /* const katex_inner = findUpClass(element, "katex_inner");
+    if (katex_inner) {
+      jQuery(".mce-katex-editor").addClass("mce-active");
+      // wrap parentNode into a katex_outer
+      enable_katex_editor(katex_inner);
+    } else {
+      jQuery(".mce-katex-editor").removeClass("mce-active");
+      disable_katex_editor();
+    } */
+  };
+
+  const findUpClass = function(el, className) {
+    if (el.classList && el.classList.contains(className)) {
+      return el;
+    }
+    while (el.parentNode) {
+      el = el.parentNode;
+      if (el.classList && el.classList.contains(className)) {
+        return el;
+      }
+    }
+    return null;
+  };
+  /*
+
+  const toWrappedContent = function(element) {
+    if (element.nodeName.toLowerCase() === "body") {
+      const parent = element;
+      const wrapper = parent.createElement("div");
+      wrapper.appendChild(element);
+      return wrapper;
+    } else {
+      const parent = element.parentNode;
+      const wrapper = document.createElement("div");
+
+      // set the wrapper as child (instead of the element)
+      parent.replaceChild(wrapper, element);
+      // set element as child of wrapper
+      wrapper.appendChild(element);
+      return wrapper;
+    }
+  }; */
+
+  const toUnwrappedContent = function(element, className) {
+    // get the element's parent node
+    const parent = findUpClass(element, className);
+    if (!parent) {
+      return null;
+    }
+    const granParent = parent.parentNode;
+
+    // move all children out of the element
+    while (parent.firstChild) {
+      granParent.insertBefore(parent.firstChild, parent);
+    }
+
+    // remove the empty element
+    granParent.removeChild(parent);
+
+    return granParent;
+  };
+
+  const create_katex_editor = () => {
+    // get the current selection
+    // const selection = (editor.selection && editor.selection.getNode()) || null;
+    /* if (!selection) {
+      return;
+    } */
+
+    editor.windowManager.open({
+      title: "Insert latex",
+      body: { type: "textbox", name: "my_textbox" },
+      style: "",
+      onsubmit: function(e) {
+        const katex_wrapper = document.createElement("span");
+        katex_wrapper.className = "katex-wrapper mceNoEditor active";
+        // katex_wrapper.style.display = "inline-block";
+        const equ = e.data.my_textbox;
+        // "\\left(x+y\\right)^n=\\sum_{k=0}^{n} \\binom{n}{k}x^ky^{n-k}";
+        katex_wrapper.innerHTML = equ.trim();
+        editor.insertContent(katex_wrapper.outerHTML);
+
+        const body = editor.dom.getRoot();
+        const spans = $$(body).find("span.katex-wrapper.active");
+        spans.each(index => {
+          const span: HTMLElement = spans[index];
+          const equation = span.innerHTML;
+          span.innerHTML = katex.renderToString(equation);
+          jQuery(span).removeClass("active");
+        });
+        // f\left(x\right)=\sqrt{2^3}
+      }
+    });
+
+    // the editor is just a div with focus in
+    /* const editor_outer = document.createElement("div");
+    const editor_outer_style = {
+      border: "1px solid #f3f3f3",
+      "min-width": "40%",
+      position: "fixed",
+      "z-index": "5",
+      top: "20px",
+      right: "10px",
+      padding: "5px",
+      background: "white",
+      "box-shadow": "1px 0px 4px #969696",
+      "max-width": "80%",
+      "text-align": "left"
+    };
+    editor_outer.className = "katex_outer";
+    Object.assign(editor_outer.style, editor_outer_style);
+
+    const editor_inner = document.createElement("span");
+    const editor_inner_style = {
+      width: "100%",
+      display: "inline"
+    };
+    editor_inner.className = "katex_inner";
+    Object.assign(editor_inner.style, editor_inner_style);
+
+    editor_outer.appendChild(editor_inner);
+    editor.insertContent(editor_outer.outerHTML);
+    setCursor(editor, editor_inner, true); */
+
+    // insert_equation(editor_inner);
+  };
+
+  /* const enable_katex_editor = (katex_inner?) => {
+    // get the actual katex formula
+    create_katex_editor();
+  }; */
+
+  const disable_katex_editor = () => {
+    const body = editor.dom.getRoot();
+    const katex_inners = $$(body).find(".katex_inner");
+    for (const item of katex_inners) {
+      toUnwrappedContent(item, "katex_outer");
+    }
+  };
+
+  editor.on("NodeChange", on_editor_node_change);
+  // editor.off("NodeChange", on_editor_node_change);
 };
 
-tinymce.PluginManager.add('latex', setup);
+tinymce.PluginManager.add("latex", setup);
 
 // tslint:disable-next-line:no-empty
 export default () => {};
